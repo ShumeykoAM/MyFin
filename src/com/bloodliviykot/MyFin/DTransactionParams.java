c1ed0d9411f58d2216904eb1639b4d8e6f1e9abd
KotBarsic <KotBarsic@8155e7df-53bb-40e7-a330-803f01620f43>
2016-12-03 13:18:37 +0000

    Продолжаю с диалогом создания транзакции

--- src/com/bloodliviykot/MyFin/DTransactionParams.java
+++ src/com/bloodliviykot/MyFin/DTransactionParams.java
@@ -1,11 +1,16 @@
 package com.bloodliviykot.MyFin;
 
 import android.annotation.SuppressLint;
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.*;
 import com.bloodliviykot.tools.widget.DialogFragmentEx;
+
+import java.util.Date;
 
 /**
  * Created by Shumeiko on 01.12.2016.
@@ -13,8 +18,19 @@ import com.bloodliviykot.tools.widget.DialogFragmentEx;
 @SuppressLint("ValidFragment")
 public class DTransactionParams
   extends DialogFragmentEx<DialogFragmentEx.I_ResultHandler<Bundle>, Bundle>
+  implements View.OnClickListener, DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener
 {
-  //private
+  public static final int YEAR_CORRECTOR = 1900;
+
+  private EditText date, time;
+  private EditText cost;
+  private Spinner cost_currency_s;
+  private TextView cost_currency_t;
+  private TextView  another_cost;
+  private Spinner another_cost_currency_s;
+  private TextView another_cost_currency_t;
+  private Spinner account;
+  private Button cancel, ok;
 
   public DTransactionParams()
   {
@@ -24,19 +40,73 @@ public class DTransactionParams
   @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
   {
-    getDialog().setTitle("Создать счет");
+    getDialog().setTitle("Оплатит продукты");
     //getDialog().getWindow().requestFeature(Window.FEATURE_NO_TITLE);
     final View v = inflater.inflate(getR_layout(), null);
-    /*
-    icon = (Spinner)v.findViewById(R.id.accounts_d_new_icon);
-    name = (EditText)v.findViewById(R.id.accounts_d_new_name);
-    b_ok     = (Button)v.findViewById(R.id.accounts_d_new_ok);     b_ok.setOnClickListener(this);
-    b_cancel = (Button)v.findViewById(R.id.accounts_d_new_cansel); b_cancel.setOnClickListener(this);
-    */
+    (date = (EditText)v.findViewById(R.id.d_tran_params_date)).setOnClickListener(this);
+    (time = (EditText)v.findViewById(R.id.d_tran_params_time)).setOnClickListener(this);
+    cost = (EditText)v.findViewById(R.id.d_tran_params_cost);
+    cost_currency_s = (Spinner)v.findViewById(R.id.d_tran_params_cost_currency_s);
+    cost_currency_t = (TextView)v.findViewById(R.id.d_tran_params_cost_currency_t);
+    another_cost = (TextView)v.findViewById(R.id.d_tran_params_another_cost);
+    another_cost_currency_s = (Spinner)v.findViewById(R.id.d_tran_params_another_cost_currency_s);
+    another_cost_currency_t = (TextView)v.findViewById(R.id.d_tran_params_another_cost_currency_t);
+    account = (Spinner)v.findViewById(R.id.d_tran_params_account);
+    (cancel = (Button)v.findViewById(R.id.d_tran_params_cancel)).setOnClickListener(this);
+    (ok = (Button)v.findViewById(R.id.d_tran_params_ok)).setOnClickListener(this);
 
     return v;
   }
 
+  @Override
+  public void onClick(View v)
+  {
+    if(v == date)
+    {
+      Date date = new Date(/*date_time*/);
+      DatePickerDialog dpd = new DatePickerDialog(v.getContext(), this, date.getYear() + YEAR_CORRECTOR,
+        date.getMonth(), date.getDate());
+      dpd.show();
+    }
+    else if(v == time)
+    {
+      Date date = new Date(/*date_time*/);
+      TimePickerDialog tpd = new TimePickerDialog(v.getContext(), this, date.getHours(),
+        date.getMinutes(), false);
+      tpd.show();
+    }
+    else if(v == cancel)
+    {
 
+    }
+    else if(v == ok)
+    {
 
+    }
+
+  }
+  @Override
+  public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth)
+  {
+    /*
+    Date date = new Date(date_time);
+    date.setYear(year - YEAR_CORRECTOR);
+    date.setMonth(monthOfYear);
+    date.setDate(dayOfMonth);
+    date_time = date.getTime();
+    updateView();
+    */
+  }
+
+  @Override
+  public void onTimeSet(TimePicker view, int hourOfDay, int minute)
+  {
+    /*
+    Date date = new Date(date_time);
+    date.setHours(hourOfDay);
+    date.setMinutes(minute);
+    date_time = date.getTime();
+    updateView();
+    */
+  }
 }
